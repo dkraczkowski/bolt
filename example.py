a2be125ac92ceacab61e4df0ced14d91cc4f68da
Dawid Kraczkowski <dawid.kraczkowski@gmail.com>
2016-03-11 17:53:44 +0000

    middlewares + cherry py change

--- example.py
+++ example.py
@@ -1,18 +1,26 @@
-from bolt.application import ServiceLocator, bolt
+from bolt.application import bolt
 from bolt.http import Request, Response
 from cherrypy import wsgiserver
-from wsgiref import simple_server
 
+@bolt.before()
+def do_something_with_request(request: Request):
+    pass
 
-@bolt.route('/test')
+@bolt.after()
+def after_middleware(request: Request, response: Response):
+    response._body += 'a'
+
+@bolt.route('/')
 class Controller:
 
-    @bolt.get('/hello_world')
+    @bolt.get('/hello')
     def action_1(self, request: Request):
 
         return Response('Hello World')
+
+
+server = wsgiserver.CherryPyWSGIServer(('0.0.0.0', 8800), bolt.ready(), numthreads=32, request_queue_size=100)
+server.start()
 '''
-server = wsgiserver.CherryPyWSGIServer(('0.0.0.0', 8800), bolt)
-server.start()'''
-httpd = simple_server.make_server('127.0.0.1', 8800, bolt)
-httpd.serve_forever()
+httpd = simple_server.make_server('127.0.0.1', 8800, bolt.ready())
+httpd.serve_forever()'''
